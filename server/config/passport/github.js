286251186e6c93959e266daebf16cec2b3525443
Amir Fakheraldeen <amir.f.aldeen@gmail.com>
2017-10-15 17:30:52 +0300

    Add GitHub passport strategy for authentication

---
+++ server/config/passport/github.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const GitHubStrategy = require('passport-github').Strategy;
+
+const config = require('../');
+const User = require('../../user.model');
+
+module.exports = new GitHubStrategy({
+    clientID: config.github.clientID,
+    clientSecret: config.github.clientSecret,
+    callbackURL: config.github.callbackURL
+  },
+  (accessToken, refreshToken, profile, done) => {
+    User.findOne({ 'github.id': profile.id }, (err, user) => {
+      if (err) return done(err);
+
+      if (user) return done(null, user);
+
+      const newUser = new User({
+        github: {
+          id: profile.id,
+          username: profile.username,
+          displayName: profile.displayName
+        }
+      });
+
+      newUser.save((err) => {
+        if (err) return done(err);
+
+        return done(null, newUser);
+      });
+    });
+  }
+);

6458293362b582d6d9637e47e715536248116f27
Amir Fakheraldeen <amir.f.aldeen@gmail.com>
2017-10-15 16:19:30 +0300

    Add user model

---
+++ server/user/user.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  github: {
+    id: { type: String, required: true, index: true },
+    username: { type: String, required: true },
+    displayName: { type: String, required: true }
+  }
+});
+
+module.exports = mongoose.model('User', userSchema, 'users');

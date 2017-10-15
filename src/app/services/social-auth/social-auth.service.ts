583a70707781eb8f2e7df6958be2720523ba3375
Amir Fakheraldeen <amir.f.aldeen@gmail.com>
2017-10-16 02:26:06 +0300

    Create social authentication service
    Created basic functionality of the social authentication service. Might
    require changes later.

---
+++ src/app/services/social-auth/social-auth.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+const TOKEN_ITEM = 'token';
+const PROFILE_ITEM = 'profile';
+
+@Injectable()
+export class SocialAuthService {
+  constructor(private router: Router) { }
+
+  login(authUrl): void {
+    const popup = window.open(authUrl);
+
+    (popup as any).onAuthCallback = (token, profile) => {
+      popup.close();
+      window.focus();
+      this.setSession(token, profile);
+    };
+
+    popup.focus();
+  }
+
+  logout(): void {
+    this.clearSession();
+    this.router.navigate(['/']);
+  }
+
+  private setSession(token: any, profile: any): void {
+    localStorage.setItem(TOKEN_ITEM, token);
+    localStorage.setItem(PROFILE_ITEM, profile);
+  }
+
+  private clearSession() {
+    localStorage.removeItem(TOKEN_ITEM);
+    localStorage.removeItem(PROFILE_ITEM);
+  }
+}

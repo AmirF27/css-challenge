be36ddbafd5e57dd43a17e2ea73c3342f67eed0a
Amir Fakheraldeen <amir.f.aldeen@gmail.com>
2017-10-18 12:10:19 +0300

    Add logout link

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SocialAuthService } from '../../services/social-auth/social-auth.service';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private socialAuth: SocialAuthService
+  ) { }
 
   ngOnInit() {
   }
-
 }

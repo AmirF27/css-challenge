98dd520425ae8e63047a46ad0d66ed9c4e5167b0
P1xt <p1xtcode@gmail.com>
2017-10-27 18:09:35 -0500

    Add challenge 065.

--- src/app/data/challenges/065/index.js
+++ src/app/data/challenges/065/index.js
@@ -1,6 +1,6 @@
 export default {
   id: 65,
-  title: '065',
+  title: '065 - Ellipses Animation',
   html: require('./065.html'),
   css: require('./065.scss'),
   js: require('!raw-loader!./065.js')

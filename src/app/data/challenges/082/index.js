a3cffb0555022c70791be3f5e3897270a00bb8c0
P1xt <p1xtcode@gmail.com>
2017-10-27 18:27:03 -0500

    Add challenge 082.

--- src/app/data/challenges/082/index.js
+++ src/app/data/challenges/082/index.js
@@ -1,6 +1,6 @@
 export default {
   id: 82,
-  title: '082',
+  title: '082 - Responding Button',
   html: require('./082.html'),
   css: require('./082.scss'),
   js: require('!raw-loader!./082.js')

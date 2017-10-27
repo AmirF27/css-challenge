36d2409dc7abe69c0a285129bf992f5ac1a25cce
P1xt <p1xtcode@gmail.com>
2017-10-27 16:38:53 -0500

    Add challenge 18.

--- src/app/data/challenges/018/index.js
+++ src/app/data/challenges/018/index.js
@@ -1,6 +1,6 @@
 export default {
   id: 18,
-  title: '018',
+  title: '018 - Elastic (CSS only)',
   html: require('./018.html'),
   css: require('./018.scss'),
   js: require('!raw-loader!./018.js')

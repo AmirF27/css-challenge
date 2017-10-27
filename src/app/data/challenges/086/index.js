fae5f945c4ffaf948c5c685ab52a9eda29def2c5
P1xt <p1xtcode@gmail.com>
2017-10-27 18:27:04 -0500

    Add challenge 086.

--- src/app/data/challenges/086/index.js
+++ src/app/data/challenges/086/index.js
@@ -1,6 +1,6 @@
 export default {
   id: 86,
-  title: '086',
+  title: '086 - Newton\'s Cradle',
   html: require('./086.html'),
   css: require('./086.scss'),
   js: require('!raw-loader!./086.js')

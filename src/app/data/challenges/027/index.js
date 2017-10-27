e96ef2e975759824459eb7420ee362e1a0432787
P1xt <p1xtcode@gmail.com>
2017-10-27 17:07:25 -0500

    Add challenge 27.

--- src/app/data/challenges/027/index.js
+++ src/app/data/challenges/027/index.js
@@ -1,6 +1,6 @@
 export default {
   id: 27,
-  title: '027',
+  title: '027 - Line on path animation',
   html: require('./027.html'),
   css: require('./027.scss'),
   js: require('!raw-loader!./027.js')

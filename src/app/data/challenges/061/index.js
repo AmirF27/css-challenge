08e97d67d76887c730a5d1c48c29a9da005311d3
P1xt <p1xtcode@gmail.com>
2017-10-27 18:09:35 -0500

    Add challenge 061.

--- src/app/data/challenges/061/index.js
+++ src/app/data/challenges/061/index.js
@@ -1,6 +1,6 @@
 export default {
   id: 61,
-  title: '061',
+  title: '061 - CSS Gradient Transition',
   html: require('./061.html'),
   css: require('./061.scss'),
   js: require('!raw-loader!./061.js')

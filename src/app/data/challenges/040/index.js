9d313210aef66a45f29b6b7db400934e4a75d620
P1xt <p1xtcode@gmail.com>
2017-10-27 17:24:11 -0500

    Add challenge 40.

--- src/app/data/challenges/040/index.js
+++ src/app/data/challenges/040/index.js
@@ -1,6 +1,6 @@
 export default {
   id: 40,
-  title: '040',
+  title: '040 - Gallery (CSS animated)',
   html: require('./040.html'),
   css: require('./040.scss'),
   js: require('!raw-loader!./040.js')

76d1cd32b49f808a7d685399c8582e42b2f9bb77
P1xt <p1xtcode@gmail.com>
2017-10-27 18:09:35 -0500

    Add challenge 066.

--- src/app/data/challenges/066/index.js
+++ src/app/data/challenges/066/index.js
@@ -1,6 +1,6 @@
 export default {
   id: 66,
-  title: '066',
+  title: '066 - Sparkle Checkbox',
   html: require('./066.html'),
   css: require('./066.scss'),
   js: require('!raw-loader!./066.js')

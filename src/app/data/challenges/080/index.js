762d42e851b466cc28ecef607876c388a14eef66
P1xt <p1xtcode@gmail.com>
2017-10-27 18:09:36 -0500

    Add challenge 080.

--- src/app/data/challenges/080/index.js
+++ src/app/data/challenges/080/index.js
@@ -1,6 +1,6 @@
 export default {
   id: 80,
-  title: '080',
+  title: '080 - Flexbox Accordeon',
   html: require('./080.html'),
   css: require('./080.scss'),
   js: require('!raw-loader!./080.js')

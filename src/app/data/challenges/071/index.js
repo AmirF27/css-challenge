02aa3f270d7182d984808b1adc93da20093d4b4d
P1xt <p1xtcode@gmail.com>
2017-10-27 18:09:35 -0500

    Add challenge 071.

--- src/app/data/challenges/071/index.js
+++ src/app/data/challenges/071/index.js
@@ -1,6 +1,6 @@
 export default {
   id: 71,
-  title: '071',
+  title: '071 - Morse Code Keyboard (CSS only)',
   html: require('./071.html'),
   css: require('./071.scss'),
   js: require('!raw-loader!./071.js')

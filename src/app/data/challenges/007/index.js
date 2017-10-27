b054032bf99826a0f67b6f09cd1f735dc41a7677
P1xt <p1xtcode@gmail.com>
2017-10-27 16:14:18 -0500

    Add challenge 7.

--- src/app/data/challenges/007/index.js
+++ src/app/data/challenges/007/index.js
@@ -1,6 +1,6 @@
 export default {
   id: 7,
-  title: '007',
+  title: '007 - Notifications, Search and Menu',
   html: require('./007.html'),
   css: require('./007.scss'),
   js: require('!raw-loader!./007.js')

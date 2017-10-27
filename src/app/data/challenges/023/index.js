32d70278940e2b6992c1b67dc6e8aa3225553ce0
P1xt <p1xtcode@gmail.com>
2017-10-27 17:06:22 -0500

    Add challenge 23.

--- src/app/data/challenges/023/index.js
+++ src/app/data/challenges/023/index.js
@@ -1,6 +1,6 @@
 export default {
   id: 23,
-  title: '023',
+  title: '023 - Animated Typography',
   html: require('./023.html'),
   css: require('./023.scss'),
   js: require('!raw-loader!./023.js')

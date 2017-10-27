39ecd6f3aa84072fb34bab9e7de528b6fe403602
P1xt <p1xtcode@gmail.com>
2017-10-27 17:49:59 -0500

    Add challenge 059.

--- src/app/data/challenges/059/index.js
+++ src/app/data/challenges/059/index.js
@@ -1,6 +1,6 @@
 export default {
   id: 59,
-  title: '059',
+  title: '059 - Slice Transition',
   html: require('./059.html'),
   css: require('./059.scss'),
   js: require('!raw-loader!./059.js')

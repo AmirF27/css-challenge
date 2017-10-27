64bba495c61f2d07c885d675d81defccaed7fff3
P1xt <p1xtcode@gmail.com>
2017-10-27 18:27:04 -0500

    Add challenge 089.

--- src/app/data/challenges/089/index.js
+++ src/app/data/challenges/089/index.js
@@ -1,6 +1,6 @@
 export default {
   id: 89,
-  title: '089',
+  title: '089 - Animated Pattern',
   html: require('./089.html'),
   css: require('./089.scss'),
   js: require('!raw-loader!./089.js')

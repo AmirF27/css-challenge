3a2dfc1f6bc3382d5c7214da397966eea8a5d404
P1xt <p1xtcode@gmail.com>
2017-10-31 11:52:44 -0500

    Fix issue with challenge list not showing completions since the last login.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,31 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {DoCheck, Component, OnInit} from '@angular/core';
 
 import { Challenge } from '../../classes/challenge';
 import { ChallengeListService } from '../../services/challenge-list/challenge-list.service';
 import { UserService } from '../../services/user/user.service';
-import { SocialAuthService } from '../../services/social-auth/social-auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, DoCheck {
   challenges: Challenge[];
   userChallenges: Map<number, any>;
 
   constructor(
     private challengeList: ChallengeListService,
-    private userService: UserService,
-    private socialAuth: SocialAuthService
+    private userService: UserService
   ) { }
 
   ngOnInit() {
     this.getChallenges();
   }
-
+  ngDoCheck() {
+    this.getChallenges();
+  }
   getChallenges(): void {
     this.challenges = this.challengeList.challenges;
-    this.userChallenges = this.userService.getUserChallengesAsMap();
+    // this.userChallenges = this.userService.getUserChallengesAsMap();
+
+    const profile = JSON.parse(localStorage.getItem('profile'));
+    if (profile !== null && profile.github !== null) {
+      this.userService.getUser(profile.github.username).then((user) => {
+        this.userChallenges = this.userService.formatChallenges(user.challengesCompleted, true ) as Map<number, any>;
+      });
+    }
+
+
   }
 }
